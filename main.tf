417cb7c22c6b68a2349059279904305ddbe95b99
nagesh66 <132870594+nagesh66@users.noreply.github.com>
2024-03-10 10:17:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "google_compute_instance" "my_instance" {
   name         = "my-instance"
   machine_type = "n1-standard-1"
   project      = "nagesh-sandbox"
+  zone         = "us-central1-a"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-10"
